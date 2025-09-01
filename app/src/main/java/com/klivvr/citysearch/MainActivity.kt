0559df7942a1942f2c30705ba72a131045019d92
nourmorgan <nour-elden.said@innovitics.com>
2025-09-02 00:17:11 +0300

    feat: add splash screen and enable edge-to-edge display

--- app/src/main/java/com/klivvr/citysearch/MainActivity.kt
+++ app/src/main/java/com/klivvr/citysearch/MainActivity.kt
@@ -2,6 +2,7 @@ package com.klivvr.citysearch
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,7 +11,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.klivvr.citysearch.core.presentation.ui.theme.CitySearchTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,7 +22,17 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        installSplashScreen()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                scrim = Color.Transparent.toArgb(),
+                darkScrim = Color.Transparent.toArgb(),
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                scrim = Color.Transparent.toArgb(),
+                darkScrim = Color.Transparent.toArgb(),
+            )
+        )
         setContent {
             CitySearchTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
